20d41a92d2170aee9c9ca298e09a3fe38ef04c4f
Rajan Saini <rajansaini691@gmail.com>
2023-03-13 17:33:12 -0700

    Add semi-supervised dataset download script

---
+++ download-tiny-images-500k.sh
@@ -0,0 +1,7 @@
+export filename=ti_500K_pseudo_labeled
+export fileid=1LTw3Sb5QoiCCN-6Y5PEKkq9C9W60w-Hi
+wget --save-cookies cookies.txt 'https://docs.google.com/uc?export=download&id='$fileid -O- \
+     | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1/p' > confirm.txt
+
+wget --load-cookies cookies.txt -O $filename \
+     'https://docs.google.com/uc?export=download&id='$fileid'&confirm='$(<confirm.txt)
